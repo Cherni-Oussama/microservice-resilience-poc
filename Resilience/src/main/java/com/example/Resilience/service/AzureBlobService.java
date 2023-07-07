9aa291b63a705890a1e22f55b593eaa222a56f32
unknown <oussama.cherni@ensi-uma.tn>
2023-07-07 20:33:08 +0100

    initial commit

---
+++ Resilience/src/main/java/com/example/Resilience/service/AzureBlobService.java
@@ -0,0 +1,76 @@
+package com.example.Resilience.service;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.models.BlobHttpHeaders;
+import com.azure.storage.blob.models.BlobStorageException;
+import com.example.Resilience.exception.FileCouldNotBeSavedToBlobException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AzureBlobService {
+
+    private final BlobContainerClient containerClient;
+
+    @Autowired
+    public AzureBlobService(BlobServiceClient blobServiceClient) {
+        String containerName = "image-container-3";
+        this.createContainerIfNotExists(blobServiceClient, containerName);
+        this.containerClient = blobServiceClient.getBlobContainerClient(containerName);
+    }
+
+    public UUID uploadImage(MultipartFile file){
+        var blobId = UUID.randomUUID();
+        log.info("Request to upload new Image to blob storage with Id {}", blobId);
+        BlobClient blobClient = containerClient.getBlobClient(String.valueOf(blobId));
+        BlobHttpHeaders blobHttpHeaders = new BlobHttpHeaders();
+        log.info(file.getOriginalFilename());
+        log.info(file.getName());
+        log.info(file.getContentType());
+        blobHttpHeaders.setContentType(file.getContentType());
+        try (InputStream inputStream = new BufferedInputStream(file.getInputStream())) {
+            blobClient.uploadWithResponse(inputStream, file.getSize(), null, blobHttpHeaders, null, null, null, null, null );
+        }
+        catch (Exception ce) {
+            log.error("Error While connecting to blob storage: " + Arrays.toString(ce.getStackTrace()));
+            throw new FileCouldNotBeSavedToBlobException("Error uploading file to blob storage", null, null, HttpStatus.BAD_REQUEST);
+        }
+        return blobId;
+    }
+
+    public Resource getImageFromBlob(UUID imageId) {
+        log.info("Request to retrieve existing image from blob storage");
+        BlobClient blobClient = containerClient.getBlobClient(String.valueOf(imageId));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        blobClient.downloadWithResponse(outputStream, null, null,null,false,null, null);
+        return new ByteArrayResource(outputStream.toByteArray());
+    }
+
+
+    private void createContainerIfNotExists(BlobServiceClient blobServiceClient ,String containerName){
+        try{
+            log.info("Creating blob container with name {}", containerName);
+            blobServiceClient.createBlobContainer(containerName);
+            log.info("CREATED: container with name {}", containerName);
+        } catch (BlobStorageException e){
+            log.info("Container {} Already exists", containerName);
+        }
+    }
+
+}
---
+++ Resilience/src/test/java/com/example/Resilience/ResilienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Resilience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
